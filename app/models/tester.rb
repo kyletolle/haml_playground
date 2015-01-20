f5ee85003009ecb58be8538f7b442fa291a8dfce
Kyle Tolle <kyle@nullsix.com>
2015-01-20 16:25:43 -0700

    Let's get rid of leading whitespace already

--- app/models/tester.rb
+++ app/models/tester.rb
@@ -1,12 +1,12 @@
 class Tester < ActiveRecord::Base
   def notes
-    <<-eos
-    This is a
-    multiline string,
-    which is helpful
+<<eos
+This is a
+multiline string,
+which is helpful
 
-    for testing!
-    eos
+for testing!
+eos
   end
 end
 
